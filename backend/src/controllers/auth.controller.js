dd9fb8172d91e27b75bda5835b1ef03179f48c2c
souptik <souptikbiswas429@gmail.com>
2025-09-03 17:30:32 +0530

    add user model

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+import asyncHandler from "../utils/asyncHandler.js"
+import APiError from "../utils/apiError.js"
+import ApiResponse from "../utils/ApiResponse.js"
+
+
+const registerUser= asyncHandler(async (req,res)=>{
+    // const {username, fullname,password,email}
+})
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,121 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto"
+import { type } from "os";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    posts:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Post"
+        }
+    ]
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
+
+//todo decode this
+userSchema.methods.generateTemporaryToken= function(){
+    const unHashedToken= crypto.randomBytes(20).toString("hex")
+
+    const hashedToken= crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex")
+
+    const tokenExpiry= Date.now()+ (20*60*1000)
+
+    return {unHashedToken,hashedToken,tokenExpiry}
+}
+
+
+export const User = mongoose.model("User", userSchema);
+
